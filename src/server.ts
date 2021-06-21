6b84e20d0afd6a7659d6cdfed88d8469654bb435
famesensor <sathit.ju@outlook.com>
2021-06-21 23:44:21 +0700

    [:hammer:]: init server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
 import fastify from 'fastify';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = fastify({ logger: true });
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, (err, address) => {
+    if (err) {
+        console.error(`server run error ${err}`);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
