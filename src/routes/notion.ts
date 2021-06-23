540996281cc100cc3deef225a01aa7d28bd448ce
famesensor <sathit.ju@outlook.com>
2021-06-24 00:24:08 +0700

    [:sparkles:]: new route notion

---
+++ src/routes/notion.ts
@@ -0,0 +1,19 @@
+import { FastifyInstance } from 'fastify';
+
+import * as notion from '../controller/notion';
+
+const notionRoute = async (app: FastifyInstance) => {
+    app.route({
+        method: 'GET',
+        url: '/task-list',
+        handler: notion.getAllApplications
+    });
+
+    app.route({
+        method: 'GET',
+        url: '/databases',
+        handler: notion.getDatabases
+    });
+};
+
+export default notionRoute;
