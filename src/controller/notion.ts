8f4e711bf5cd3d0b43a0f245fe58a9d5dbe17fe4
famesensor <sathit.ju@outlook.com>
2021-06-25 00:48:12 +0700

    [:hammer:]: update get all application

--- src/controller/notion.ts
+++ src/controller/notion.ts
@@ -1,5 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 
+import { HandlerError } from '../utils/errors/errors';
 import { notion } from '../third-party/client-notion';
 
 const getAllApplications = async (req: FastifyRequest, reply: FastifyReply) => {
@@ -8,21 +9,40 @@ const getAllApplications = async (req: FastifyRequest, reply: FastifyReply) => {
             database_id: process.env.NOTION_DATABASE!
         });
 
-        console.log(data);
+        const notions = data.results.map((item) => {
+            console.log(item);
+            return {
+                id: item.id,
+                create_date: item.created_time,
+                type: item.properties.Type,
+                status: item.properties.Stats,
+                priorty: item.properties.Priorty,
+                desc: item.properties.Name
+            };
+        });
+
+        return notions;
     } catch (error) {
         console.log(error);
+        HandlerError(error);
     }
+};
+
+const getApplication = async (req: FastifyRequest, reply: FastifyReply) => {
+    console.log(req.params.page_id!);
+    return null;
 };
 
 const getDatabases = async (req: FastifyRequest, reply: FastifyReply) => {
     try {
         const databases = await notion.databases.list();
-
         console.log(databases);
+
+        return null;
     } catch (error) {
         console.log(error);
-        throw new Error(error);
+        HandlerError(error);
     }
 };
 
-export { getDatabases, getAllApplications };
+export { getDatabases, getAllApplications, getApplication };
