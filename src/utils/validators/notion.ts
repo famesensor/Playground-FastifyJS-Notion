9191b585b5cea18784b656e439ebab0a5e86dcfc
famesensor <sathit.ju@outlook.com>
2021-06-27 00:47:27 +0700

    [:hammer:]: update validate create page

--- src/utils/validators/notion.ts
+++ src/utils/validators/notion.ts
@@ -1,1 +1,28 @@
-export const createPage = {};
+export const createPage = {
+    body: {
+        type: 'object',
+        required: ['title', 'type_desc', 'status', 'priorty', 'start'],
+        properties: {
+            title: {
+                type: 'string'
+            },
+            type_desc: {
+                type: 'string',
+                enum: ['Self-learning', 'Meeting', 'Project', 'Myself']
+            },
+            date: {
+                start: {
+                    type: 'string'
+                },
+                end: {
+                    type: 'string'
+                }
+            },
+            status: {
+                type: 'string',
+                enum: ['To Do', 'Doing', 'Every day', 'Done']
+            },
+            priorty: { type: 'string' }
+        }
+    }
+};
